966fba5f68383be494789410974687ac0892310a
sidaroz <sidarozbek96@gmail.com>
2022-04-27 13:02:36 +0100

    working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home/Home';
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import RepoInfo from "./components/RepoInfo/RepoInfo";
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/:username/:repo" element={<userInfo />} />
-      </Routes>
-    </div>
+      <div className="App">
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/:username/:repo" element={<RepoInfo />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,59 +1,52 @@
-import React, {useState, useEffect} from 'react';
-import { Link, useNavigate, useLocation } from 'react-router-dom';
-import './Home.css';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Home.css";
 
 function Home() {
+  const [username, setUsername] = useState("sidaroz");
+  const [repos, setRepos] = useState([]);
+  const navigate = useNavigate();
 
-    const [username, setUsername] = useState('')
-    const [repos, setRepos] = useState([])
-    // const state = useLocation()
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+    setUsername(e.target.user.value);
+  };
 
-    // useEffect(() => {
-    //     setUsername(state.state);
-    // }, [])
-
-    const handleSubmitForm = (e) => {
-        e.preventDefault();
-        setUsername(e.target.user.value)
+  useEffect(() => {
+    async function fetchData() {
+      const user = await fetch(
+        `https://api.github.com/users/${username}/repos`
+      );
+      const data = await user.json();
+      setRepos(data);
     }
 
+    fetchData();
+  }, [username]);
 
-    useEffect(() => {
-        async function fetchData(){
-            const user = await fetch(`https://api.github.com/users/${username}/repos`)
-            const data = await user.json();
-            setRepos(data)
-        }
-
-        fetchData()
-    }, [username])
-
-
-
-    // const repositories = repos.map((repo, i) => {
-    //     return (
-    //         <div key={i}>
-    //             <Link to={repo.id} >{repo.name}</Link>
-    //         </div>
-    //     )
-
-    // })
+  const repositories = repos.map((repo, i) => {
+    return (
+      <div key={i}>
+        <a onClick={() => navigate(`/${username}/${repo.id}`)}>{repo.name}</a>
+      </div>
+    );
+  });
 
   return (
-    <div className='content'>
-        <form onSubmit={handleSubmitForm}>
-            <input name="user" required placeholder='Enter Username Here'></input>
-            <input type="submit"></input>
-        </form>
+    <div className="content">
+      <form onSubmit={handleSubmitForm}>
+        <input name="user" required placeholder="Enter Username Here"></input>
+        <input type="submit"></input>
+      </form>
 
-        <div className='userInfo'>
-            <h1> {username} </h1>
-        </div>
+      <div className="userInfo">
+        <h1> {username} </h1>
+      </div>
 
-        {/* <div className='repoInfo'></div>
-        <h2> {repositories} </h2> */}
+      <div className="repoInfo"></div>
+      <h3> {repositories} </h3>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
